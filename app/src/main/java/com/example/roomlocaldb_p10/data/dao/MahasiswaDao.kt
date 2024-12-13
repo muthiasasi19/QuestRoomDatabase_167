0780398e82c17fbfd73c4d8e9fb8e2a0a047feae
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-13 12:05:13 +0700

    Menambahkan code untuk mendukung operasi operasi CRUD pada file MahasiswaDao

--- app/src/main/java/com/example/roomlocaldb_p10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/data/dao/MahasiswaDao.kt
@@ -1,8 +1,12 @@
 package com.example.roomlocaldb_p10.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 //suspend: bisa digunakan di insert, update, delete
 
@@ -11,4 +15,21 @@ interface MahasiswaDao {
 
     @Insert
     suspend fun  insertMahasiswa(mahasiswa: Mahasiswa)
+
+    // Untuk mengambil semua data mahasiswa yang tersimpan berdasarkan nama secara ascending (ASC)
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+    // untuk mengambil data mahasiswa bedasarkan NIM
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim : String) : Flow<Mahasiswa>
+
+    // Untuk menghapus data mahasiswa tertentu dari database
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    // untuk memperbarui informasi mahasiswa yang sudah ada di database
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
 }
