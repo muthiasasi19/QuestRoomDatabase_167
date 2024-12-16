208b1690781a4037986272d183c690bc5e3645ae
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 18:26:29 +0700

    Menampilkan detail mahasiswa, indikator loading saat data dimuat, dan konfirmasi untuk menghapus data saat tombol "Delete" ditekan.

--- app/src/main/java/com/example/roomlocaldb_p10/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,15 +1,23 @@
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,6 +25,69 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
+import com.example.roomlocaldb_p10.ui.viewmodel.DetailUiState
+import com.example.roomlocaldb_p10.ui.viewmodel.toMahasiswaEntity
+
+@Composable
+fun BodyDetailMhs(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = {}
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when{
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                //menampilkan indikator loading
+                CircularProgressIndicator()
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column (modifier = modifier.fillMaxWidth().padding(16.dp))
+            {
+                ItemDetailMhs(
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier =  Modifier.padding(8.dp))
+                Button(onClick = {
+                    deleteConfirmationRequired = true
+                }, modifier = Modifier.fillMaxWidth())
+                {
+                    Text(text = "Delete", fontSize = 18.sp)
+                }
+
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel =  {
+                            deleteConfirmationRequired = false
+                        }, modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUIEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailMhs(
