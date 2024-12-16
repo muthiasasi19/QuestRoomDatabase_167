8cbbb59ffb6070a62f5c7641f00ef08dea7ec825
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 16:21:43 +0700

    mengambil data mahasiswa berdasarkan NIM dari savedStateHandle dan mengubahnya menjadi UI state yang berisi detail mahasiswa dan status loading.

--- app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
@@ -5,13 +5,26 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
 import com.example.roomlocaldb_p10.repository.RepositoryMhs
+import com.example.roomlocaldb_p10.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ): ViewModel()
 {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
 }
 
 
