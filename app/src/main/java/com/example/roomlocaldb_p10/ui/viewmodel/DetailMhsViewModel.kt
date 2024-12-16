5771b96a0d5e0e15b08951b4b250f599d5d625ab
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 16:18:10 +0700

    membuat ViewModel untuk mengelola data mahasiswa dengan mengambil data dari SavedStateHandle dan repository

--- app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.roomlocaldb_p10.ui.viewmodel
 
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
+import com.example.roomlocaldb_p10.repository.RepositoryMhs
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+): ViewModel()
+{
+
+}
 
 
 data class DetailUiState(
