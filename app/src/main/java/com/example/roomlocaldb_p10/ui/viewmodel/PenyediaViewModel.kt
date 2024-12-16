9aed50740d42061a602c1ef4d0f75d4bce9401fb
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 17:20:06 +0700

    Menginisialisasi tiga ViewModel berbeda (HomeMhsViewModel, DetailMhsViewModel, UpdateMhsViewModel) dengan menyediakan repository dan state yang diperlukan.

--- app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.roomlocaldb_p10.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomlocaldb_p10.KrsApp
+import com.example.roomlocaldb_p10.ui.view.mahasiswa.HomeMhsViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -12,10 +14,30 @@ object PenyediaViewModel {
             MahasiswaViewModel(
                 krsApp().containerApp.repositoryMhs
             )
+
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
         }
     }
 
 }
+
 
 fun CreationExtras.krsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
