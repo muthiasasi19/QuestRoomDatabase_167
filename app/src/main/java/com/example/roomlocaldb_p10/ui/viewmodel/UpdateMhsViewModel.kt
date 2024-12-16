8d0aeb87c48da5aafdea77bbe1187d6fac6c180b
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 16:42:04 +0700

    Untuk memvalidasi dan memperbarui data mahasiswa, kemudian memperbarui status tampilan dengan pesan sukses atau gagal, tergantung pada hasil operasi.

--- app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -30,6 +30,33 @@ class UpdateMhsViewModel (
                 .toUIStateMhs()
         }
     }
+    fun updateData(){
+        val currentEvent = updateUiState.mahasiswaEvent
+
+        if (validateFields()){
+            viewModelScope.launch {
+                try{
+                    repositoryMhs.updateMhs(currentEvent.toMahasiswaEntity())
+                    updateUiState = updateUiState.copy(
+                        snackBarMessage = "Data berhasil diupdate",
+                        mahasiswaEvent = MahasiswaEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                    println("snackBarMessage diatur: ${updateUiState.snackBarMessage}")
+                } catch (e: Exception){
+                    updateUiState = updateUiState.copy(
+                        snackBarMessage = "Data gagal diupdate"
+                    )
+                }
+            }
+        } else{
+            updateUiState = updateUiState.copy(
+                snackBarMessage = "Data gagal diupdate"
+            )
+        }
+    }
+
+
 
 }
 
