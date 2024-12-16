66b1005d86383336be5900718f4b89adcba34394
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 16:31:21 +0700

    Mengatur status loading dan kesalahan saat mengambil data, serta menyediakan fungsi untuk menghapus data mahasiswa.

--- app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,14 +1,20 @@
 package com.example.roomlocaldb_p10.ui.viewmodel
 
-
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
 import com.example.roomlocaldb_p10.repository.RepositoryMhs
 import com.example.roomlocaldb_p10.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -25,6 +31,33 @@ class DetailMhsViewModel (
                 isLoading = false,
             )
         }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue =
+            DetailUiState(isLoading = true)
+        )
+
+    fun deleteMhs(){
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
 }
 
 
