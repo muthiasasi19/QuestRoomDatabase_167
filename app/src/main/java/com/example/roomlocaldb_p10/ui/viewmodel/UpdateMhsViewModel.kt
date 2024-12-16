d2e8b47b53b7c5216dbccbbd66c0f76003ece13f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 16:36:39 +0700

    Mengambil data mahasiswa berdasarkan NIM dan menyimpannya untuk pembaruan tampilan.

---
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.roomlocaldb_p10.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomlocaldb_p10.repository.RepositoryMhs
+import com.example.roomlocaldb_p10.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    var updateUiState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
+}
+
