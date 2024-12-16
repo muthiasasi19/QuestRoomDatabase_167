953715c093b5db6c2892c6ad1fe9260d01fdacfc
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 15:19:38 +0700

    Menambahkan code (CRUD) pda file codingan RepositoryMhs untuk mengelola data mahasiswa

--- app/src/main/java/com/example/roomlocaldb_p10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/repository/RepositoryMhs.kt
@@ -1,7 +1,21 @@
 package com.example.roomlocaldb_p10.repository
 
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //untuk mendapatkan semua data mahasiswa dalam bentuk aliran data (flow)
+    fun getAllMhs (): Flow<List<Mahasiswa>>
+
+    // untuk mengambil data mahasiswa berdasarkan NIM
+    fun getMhs(nim : String) : Flow<Mahasiswa>
+
+    // untuk menghapus data mahasiswa
+    suspend fun  deleteMhs(mahasiswa: Mahasiswa)
+
+    // untuk memperbarui data mahasiswa didatabase
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
 }
