210f0e65a2f656db4b4d6b3ff0cd903d1afcc9f7
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 15:54:30 +0700

    untuk menyimpan data dan status halaman "Home"

---
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+
+import com.example.roomlocaldb_p10.data.entity.Mahasiswa
+
+
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
