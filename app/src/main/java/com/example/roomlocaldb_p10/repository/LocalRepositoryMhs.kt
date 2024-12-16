210e12ef470593c6ef712d2e306be1fea4867249
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 15:35:58 +0700

    Menambahkan code (CRUD) pda file codingan LocalRepositoryMhs untuk menghubungkan MahasiswaDao dengan operasi yang sudah didefinisikan di RepositoryMhs

--- app/src/main/java/com/example/roomlocaldb_p10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,30 @@ package com.example.roomlocaldb_p10.repository
 
 import com.example.roomlocaldb_p10.data.dao.MahasiswaDao
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private  val mahasiswaDao: MahasiswaDao
-) : RepositoryMhs { // yg menandakan implementasi interface
+) : RepositoryMhs {
+    // yg menandakan implementasi interface
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+
     }
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+    //mengambil data mahasiswa berdasarkan nim
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+    //menghapus data mahasiswa
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+    //memperbarui data mahasiswa dalam database
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+
 }
