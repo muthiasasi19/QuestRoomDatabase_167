d53fbbc15604adce9b7e1ce49ea51603c28d794d
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-16 16:10:47 +0700

    menyimpan status tampilan detail mahasiswa dan memeriksa apakah data mahasiswa kosong atau tidak

--- app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb_p10/ui/viewmodel/DetailMhsViewModel.kt
@@ -4,7 +4,19 @@ package com.example.roomlocaldb_p10.ui.viewmodel
 import com.example.roomlocaldb_p10.data.entity.Mahasiswa
 
 
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+{
+    val isUIEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
 
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
 //DATA CLASS untuk menampung data yang akan ditampilkan di UI
 
 //Memindahkan data dari entity ke UI
